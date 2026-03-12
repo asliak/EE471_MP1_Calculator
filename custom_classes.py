ac054fb7f28d8fd4b97f4d03cb33b4c452b4d9be
asliak <aksakalaslinur@gmail.com>
2026-03-12 21:49:17 +0300

    merged mul manually

--- custom_classes.py
+++ custom_classes.py
@@ -2,16 +2,12 @@ class Calculator(object):
     def __init__(self):
         self._current_val = 0
     
-<<<<<<< HEAD
-<<<<<<< HEAD
     def add(self, x, y):
         self._current_val = x + y
-=======
+
     def subtract(self, x, y):
         self._current_val = x - y
->>>>>>> feat/sub
-=======
+
     def multiply(self, x, y):
         self._current_val = x * y
->>>>>>> feat/mul
         return self._current_val
