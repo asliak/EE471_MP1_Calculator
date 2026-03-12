43a69ceae47bb52acb5c8013c1569bffa17b1bc2
asliak <aksakalaslinur@gmail.com>
2026-03-12 21:42:39 +0300

    feat: div method implemented

--- custom_classes.py
+++ custom_classes.py
@@ -1,3 +1,9 @@
 class Calculator(object):
     def __init__(self):
         self._current_val = 0
+    
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Divider can't be 0!")
+        self._current_val = x / y
+        return self._current_val
