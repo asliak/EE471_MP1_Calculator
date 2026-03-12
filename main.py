ee5d4ba92d3dc40d10da53bd35857860aa1df978
asliak <aksakalaslinur@gmail.com>
2026-03-12 21:59:28 +0300

    final version

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from custom_classes import Calculator
 
 calc = Calculator()
 
-print(calc._current_val)
+step1 = calc.add(10, 5)
+step2 = calc.multiply(step1, 2)
+step3 = calc.divide(step2, 3)
+
+print(f"Result: {step3}")
+print(f"{calc._current_val}")
