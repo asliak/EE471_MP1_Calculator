06e245527708081070b50801a8b9435c297c9b04
asliak <aksakalaslinur@gmail.com>
2026-03-12 21:56:30 +0300

    merged all features

--- custom_classes.py
+++ custom_classes.py
@@ -2,7 +2,6 @@ class Calculator(object):
     def __init__(self):
         self._current_val = 0
     
-<<<<<<< HEAD
     def add(self, x, y):
         self._current_val = x + y
 
@@ -11,10 +10,8 @@ class Calculator(object):
 
     def multiply(self, x, y):
         self._current_val = x * y
-=======
     def divide(self, x, y):
         if y == 0:
             raise ValueError("Divider can't be 0!")
         self._current_val = x / y
->>>>>>> feat/div
         return self._current_val
